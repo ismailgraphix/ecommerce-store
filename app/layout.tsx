1ed04621f5586a62ef4b929b8c10982b99327a68
ismailgraphix <ismailgrapix@gmail.com>
2024-07-13 14:32:23 +0100

    contemplating

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Footer from "@/components/footer";
 import Navbar from '@/components/navbar';
 import ModalProvider from "@/providers/modal-provider";
 import ToastProvider from "@/providers/toast-provider";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const font = Urbanist({ subsets: ["latin"] });
 
@@ -21,10 +22,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        
+            
         <ModalProvider/>
         <ToastProvider />
         <Navbar/>
         {children}
+       
         <Footer/>
         </body>
     </html>
--- components/navbar-action.tsx
+++ components/navbar-action.tsx
@@ -6,6 +6,8 @@ import { useEffect, useState } from "react";
 import useCart from "@/hooks/use-cart";
 import { useRouter } from "next/navigation";
 
+
+
 const NavbarActions = () => {
     const [isMounted, setIsMounted]  = useState(false);
 
@@ -21,6 +23,7 @@ const NavbarActions = () => {
         <div className="ml-auto flex items-center gap-x-4">
 
             <Button onClick={() => router.push("/cart")} className="flex items-center rounded-full bg-black px-4 py-2">
+               
              <ShoppingBag
              size={20}
              color='white' />
